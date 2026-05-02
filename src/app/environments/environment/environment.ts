54a34e9568663d21aebbbbef049bc47259e25421
Bruce Castrojo <20244215@s.ubaguio.edu>
2026-05-02 12:04:46 +0800

    chore: commit environment.ts with placeholder key for easy setup

---
+++ src/app/environments/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  openWeatherMapKey: 'a70ce70db30814a77b78b52931263f0f',
+  openWeatherMapBase: 'https://api.openweathermap.org/data/2.5',
+  restCountriesBase: 'https://restcountries.com/v3.1' // a
+};
